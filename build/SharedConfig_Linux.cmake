3b6ef7c4dc04adf474f925a8ebe28fe29e57364a
adchawla <adchawla@gmail.com>
2015-02-11 03:42:18 +0530

    Porting changes on Linux
    Signed-off-by: adchawla <adchawla@gmail.com>

--- build/SharedConfig_Linux.cmake
+++ build/SharedConfig_Linux.cmake
@@ -1,14 +1,12 @@
 add_definitions(-DUNIX_ENV=1)
 
 set (OS_FOLDER_NAME "i80386linux_x64")
-if ( (${CMAKE_BUILD_TYPE} STREQUAL "Debug") OR (${CMAKE_BUILD_TYPE} STREQUAL "debug") )
-    set (BUILD_CONFIGURATION "debug")
-else()
-    set (BUILD_CONFIGURATION "release")
-endif()
+string(TOLOWER ${CMAKE_BUILD_TYPE} BUILD_CONFIGURATION)
+
+set(STATIC_LIB_PREFIX "lib")
+set(STATIC_LIB_EXTENSION "a")
 
 set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
-set (CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/${BUILD_CONFIGURATION})
 
 function( GetFullSharedLibraryName shortName fullName )
 	set( fullName ${shortName}.dll)
--- build/SharedConfig_Win.cmake
+++ build/SharedConfig_Win.cmake
@@ -3,6 +3,9 @@ add_definitions(-DWIN_ENV=1 -D_CRT_SECURE_NO_WARNINGS=1 -D_SCL_SECURE_NO_WARNING
 set (OS_FOLDER_NAME "windows_x64")
 set (BUILD_CONFIGURATION "$(Configuration)")
 
+set(STATIC_LIB_PREFIX "")
+set(STATIC_LIB_EXTENSION "lib")
+
 function( GetFullSharedLibraryName shortName fullName )
 	set( fullName ${shortName}.dll)
 endfunction( GetFullSharedLibraryName )
