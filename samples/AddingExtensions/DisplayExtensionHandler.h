fb912b01e8113ad1eb3728a7f3fced4f9dd27b02
abjindal <abjindal@adobe.com>
2016-05-27 18:06:11 +0530

    Some output formatting related changes

--- samples/AddingExtensions/DisplayExtensionHandler.h
+++ samples/AddingExtensions/DisplayExtensionHandler.h
@@ -24,17 +24,30 @@ namespace NS_DisplayExtension {
 	{
 	public:
 
+		/* Initialize the display object present in the extension handler object*/
 		virtual bool BeginCustomData();
+
+		/* Clear the display object present in the extension handler object*/
 		virtual spICustomData EndCustomData();
 
+
+		/*Implement this function if the extension contains structure objects*/
 		virtual bool BeginStructure( const std::string & structureName );
+
+		/*Implement this function if the extension contains structure objects*/
 		virtual bool EndStructure( const std::string & structureName );
 
+		/*Implement this function if the extension contains array objects*/
 		virtual bool BeginArray( const std::string & arrayName );
+
+
+		/*Implement this function if the extension contains array objects*/
 		virtual bool EndArray( const std::string & arrayName );
 
+		/*Implement this function if the extension contains key-value pairs*/
 		virtual bool AddKeyValuePair( const std::string & key, const std::string & value );
 
+		/*Serializing the extension*/
 		virtual bool Serialize( const spICustomData & data, const spICustomDataHandler & customDataCreator ) const;
 
 	protected:
