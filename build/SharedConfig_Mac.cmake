1056831c081c5c8e3856511d6b604d408c62deec
Abhishek Jindal <abjindal@abhi-osx.corp.adobe.com>
2016-06-01 18:12:45 +0530

    Fixing build issues on mac, specifically fixing project build directories and link directories for samples, private, api test cases and unit test cases

--- build/SharedConfig_Mac.cmake
+++ build/SharedConfig_Mac.cmake
@@ -1,7 +1,7 @@
 add_definitions(-DMAC_ENV=1)
 
 set (OS_FOLDER_NAME "macintosh/intel_64")
-set (BUILD_CONFIGURATION ${CMAKE_BUILD_TYPE})
+set (BUILD_CONFIGURATION ${CMAKE_CFG_INTDIR})
 
 set(STATIC_LIB_PREFIX "lib")
 set(STATIC_LIB_EXTENSION "a")
