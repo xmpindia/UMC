e1d4494700ffc362e49baa8cd869521c477a0223
abjindal <abjindal@adobe.com>
2016-07-18 11:55:01 +0530

    Changing copyright notices to ApacheLiences for opensourcing the SDK

--- public/include/interfaces/IAudioSource.h
+++ public/include/interfaces/IAudioSource.h
@@ -1,14 +1,19 @@
 #ifndef IAudioSource_h__
 #define IAudioSource_h__ 1
 
-// =================================================================================================
-// ADOBE SYSTEMS INCORPORATED
-// Copyright 2015 Adobe Systems Incorporated
-// All Rights Reserved
-//
-// NOTICE:  Adobe permits you to use, modify, and distribute this file in accordance with the terms
-// of the Adobe license agreement accompanying it.
-// =================================================================================================
+/*
+*  Copyright 2016 Adobe Systems Incorporated. All rights reserved.
+*  This file is licensed to you under the Apache License, Version 2.0 (the "License");
+*  you may not use this file except in compliance with the License. You may obtain a copy
+*  of the License at http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software distributed under
+*  the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+*  OF ANY KIND, either express or implied. See the License for the specific language
+*  governing permissions and limitations under the License.
+*
+*/
+
 
 #include "UMCFwdDeclarations.h"
 #include "ISource.h"
